f9c82e881e5588f70b6a4c327454541edc9f701e
mariano <marianoledesma@alumno.unlam.edu.ar>
2021-07-03 15:45:31 -0300

    changes

--- app/src/main/java/com/example/navigationdrawer/ui/adapter/AdapterHome.kt
+++ app/src/main/java/com/example/navigationdrawer/ui/adapter/AdapterHome.kt
@@ -42,7 +42,7 @@ class AdapterHome(private val items: List<Meals>, private val itemClickListener:
         itemsMainBinding.cardView.setOnClickListener{
             val bundle=Bundle()
             bundle.putParcelable(AdapterMeals.MEALS_ITEMS,items[position])
-            it.findNavController().navigate(R.id.action_nav_home_to_mealDetailFragment,bundle)
+            it.findNavController().navigate(R.id.action_nav_home_to_detailHomeFragment,bundle)
         }
 
     }
