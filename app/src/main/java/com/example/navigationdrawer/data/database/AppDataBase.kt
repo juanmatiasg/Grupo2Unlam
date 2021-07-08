944aafc52d1e082b171799440601a6d6d4464af1
mariano <marianoledesma@alumno.unlam.edu.ar>
2021-07-08 14:23:37 -0300

    cambios de version en la bd

--- app/src/main/java/com/example/navigationdrawer/data/database/AppDataBase.kt
+++ app/src/main/java/com/example/navigationdrawer/data/database/AppDataBase.kt
@@ -25,7 +25,7 @@ abstract class AppDataBase : RoomDatabase() {
             INSTANCE = INSTANCE ?: Room.databaseBuilder(
                 context.applicationContext,
                 AppDataBase::class.java,
-                "DBMeals_API_v1.0.17"
+                "DBMeals_API_v1.0.18"
             )
                 .build()
             return INSTANCE!!
