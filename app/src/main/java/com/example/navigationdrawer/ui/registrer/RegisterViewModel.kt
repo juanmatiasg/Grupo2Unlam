1e9f17c191e20a3bc12b2d133c88700f3176f641
Mariano Ledesma <marianoledesma@emergencias.com.ar>
2021-09-13 20:18:00 -0300

    initial config into fil

---
+++ app/src/main/java/com/example/navigationdrawer/ui/registrer/RegisterViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.navigationdrawer.ui.registrer
+
+import androidx.lifecycle.ViewModel
+import com.example.navigationdrawer.domain.Repo
+
+class RegisterViewModel(private val repo: Repo) : ViewModel() {
+
+}
